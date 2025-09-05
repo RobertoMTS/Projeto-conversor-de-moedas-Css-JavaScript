21504dffe0fb5c975cc8a4e5acabfa3dd63d6c71
RobertoMTS <robertotrafegopago@gmail.com>
2025-09-04 22:05:27 -0300

    Colocando JS mais Logica

---
+++ script.js
@@ -0,0 +1,37 @@
+const valor = document.querySelector('#valor'); 
+const de = document.querySelector('#de');
+const para = document.querySelector('#para');
+const resultado = document.querySelector('#resultado');
+const conversor = document.querySelector('#conversor');
+
+
+const taxas = {
+    brl: {brl: 1, usd: 0.18, eur: 0.16},
+    usd: {brl: 5.47 , usd: 1, eur: 0.86},
+    eur: {usd: 1.16, eur: 1, brl: 6.37},
+}
+
+
+conversor.addEventListener('click', () => {
+    const valorInserido = parseFloat (valor.value);
+    const valorDe = de.value; 
+    const valorPara = para.value; 
+
+    console.log(valorInserido)
+    console.log(valorDe)
+    console.log(valorPara)
+
+
+    if(isNaN(valorInserido) || valorInserido <= 0){
+        resultado.textContent = 'digite um valor valido';
+        return;
+    };
+
+    const taxa = taxas[valorDe][valorPara];
+
+    const conversao = valorInserido * taxa;
+
+    resultado.textContent = `${valorDe.toUpperCase()} ${valorInserido} = ${valorPara.toUpperCase()} ${conversao}`;
+
+});
+
